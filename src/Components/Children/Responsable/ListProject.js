5f0fceeb5d87bf4cf45966e4b80df08b6c167a84
AKKA.ZAKARIA <akkazakaria.stud@gmail.com>
2022-09-08 13:45:44 +0100

    Création de button - delete project

--- src/Components/Children/Responsable/ListProject.js
+++ src/Components/Children/Responsable/ListProject.js
@@ -10,20 +10,34 @@ const ListProject = (props) => {
     const [bullInfo, setInfo] = useState(false);
     const [projects, setProjects] = useState([]); // pour récuperer la liste des projets 
     const [projectDetails, setProjectDetails] = useState(); // pour récuperer les details d'un seule projet et affiché dans la section detail
-    const [createur,setCreateur] = useState()
+    const [createur, setCreateur] = useState()
     useEffect(() => {
-        
+
         fetch('http://localhost:8080/project/list/all')
             .then(res => res.json())
             .then(res => setProjects(res))
-    },[])
+    }, [])
 
     useEffect(() => {
-        
-         setProjects(props.proList)
 
-    },[props.proList])
-    
+        setProjects(props.proList)
+
+    }, [props.proList])
+
+    const deleteProject = () => {
+
+        if (window.confirm("Etes-vous sur ? ^ ^")) {
+
+            fetch('http://localhost:8080/project/delete/' + projectDetails.idProject)
+                .then(() => {
+                    fetch('http://localhost:8080/project/list/all')
+                        .then(res => res.json())
+                        .then(res => setProjects(res))
+                    setInfo(false);
+                })
+        }
+    }
+
 
     return (
         <div className='listProject'>
@@ -31,13 +45,13 @@ const ListProject = (props) => {
                 <h1>PROJECT</h1>
                 <h4>LIST</h4>
             </div>
-            {projects.map((project, index) => <Project key={index} data={project} setProjectDetails={setProjectDetails} setInfo={setInfo} sign={false} setCreateur={setCreateur}/>)}
+            {projects.map((project, index) => <Project key={index} data={project} setProjectDetails={setProjectDetails} setInfo={setInfo} sign={false} setCreateur={setCreateur} />)}
             {bullInfo &&
                 <section className='info_project'>
                     <header>
                         <i className="fa-solid fa-minus" onClick={() => setInfo(false)}></i>
-                        <button>Update</button>
-                        <button>Delete</button>
+                        <button hidden>Update</button>
+                        <button onClick={deleteProject}>Delete</button>
                     </header>
                     <section>
                         <h1>{projectDetails.titleProject === null ? 'Project' : projectDetails.titleProject}</h1>
--- src/Components/Parent/Home.js
+++ src/Components/Parent/Home.js
@@ -1,7 +1,7 @@
 
 import '../../Style/Home/Home.css';
 import Menu from '../Children/Menu';
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import ListProject from '../Children/Responsable/ListProject';
 import CreateProject from '../Children/Responsable/CreateProject';
 import StateProject from '../Children/Responsable/StateProject';
@@ -9,56 +9,56 @@ import CreateSprint from '../Children/Responsable/CreateSprint';
 import Profil from '../Children/Responsable/Profil';
 
 
-const Home = (props) => 
-{   
+const Home = (props) => {
     const [search, setSearch] = useState('');
     const [proList, setProjects] = useState([]); // pour récuperer la liste des projets
-    const [operation,setOperation] = useState("list_p"); // pour passer d'une operation a l'autre list/creation/consultation.. de projet
-    const [showSearch,setShowSearch] = useState(true); // pour afficher la boite de recherche
-    const [showNotification,setShowNotification] = useState(false); // pour afficher les notifications
-    const [showContact,setShowContact] = useState(false); // pour afficher la boite de contact
+    const [operation, setOperation] = useState("list_p"); // pour passer d'une operation a l'autre list/creation/consultation.. de projet
+    const [showSearch, setShowSearch] = useState(true); // pour afficher la boite de recherche
+    const [showNotification, setShowNotification] = useState(false); // pour afficher les notifications
+    const [showContact, setShowContact] = useState(false); // pour afficher la boite de contact
 
     useEffect(() => {
 
-            if(search !== '')
-            {
-                fetch('http://localhost:8080/project/search/' + search)
+        if (search !== '') {
+            fetch('http://localhost:8080/project/search/' + search)
                 .then(res => res.json())
                 .then(res => setProjects(res))
-            }
-            else{
+        }
+        else {
 
-                fetch('http://localhost:8080/project/list/all')
+            fetch('http://localhost:8080/project/list/all')
                 .then(res => res.json())
                 .then(res => setProjects(res))
-            }
-        
-    },[search])
+        }
+
+    }, [search])
 
     return (
         <div className='home'>
-            <section className='navigation_section'> <Menu setPage={props.setPage} setOperation={setOperation} setConnect={props.setConnect} setShowSearch={setShowSearch}/> </section>
+            <section className='navigation_section'> <Menu setPage={props.setPage} setOperation={setOperation} setConnect={props.setConnect} setShowSearch={setShowSearch} /> </section>
             <section className='work_section'>
                 <header>
                     <div className='search_filter'>
                         {showSearch &&
-                        <form className='search_form'>
-                            <i className="fa-solid fa-circle-notch"></i>
-                            <input type="search" placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)}/>
-                        </form>
+                            <form className='search_form'>
+                                <i className="fa-solid fa-circle-notch"></i>
+                                <input type="search" placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)} />
+                            </form>
                         }
                     </div>
-                    <div className='notification_msg'>        
-                        <i className="fa-solid fa-location-arrow" onClick={() => setShowContact(!showContact)}></i>
-                        <i className="fa-solid fa-bell" onClick={() => setShowNotification(!showNotification)}><div></div></i>
+                    <div className='notification_msg'>
+                        {/* <i className="fa-solid fa-location-arrow" onClick={() => setShowContact(!showContact)}></i>
+                        <i className="fa-solid fa-bell" onClick={() => setShowNotification(!showNotification)}><div></div></i> */}
+                        <i className="fa-solid fa-location-arrow"></i>
+                        <i className="fa-solid fa-bell"><div></div></i>
                     </div>
                 </header>
-                <section className='change_section'> 
-                    {operation === 'list_p' ? <ListProject proList={proList}/> : operation === 'create_p'
-                                            ? <CreateProject owner={props.owner} /> : operation === 'sprint_p' 
-                                            ? <CreateSprint owner={props.owner} /> : operation === 'consult_p'
-                                            ? <StateProject setShowSearch={setShowSearch} proList={proList}/> : operation === 'profil'
-                                            ? <Profil  owner={props.owner} setOwner={props.setOwner}/> : null} 
+                <section className='change_section'>
+                    {operation === 'list_p' ? <ListProject proList={proList} /> : operation === 'create_p'
+                        ? <CreateProject owner={props.owner} /> : operation === 'sprint_p'
+                            ? <CreateSprint owner={props.owner} /> : operation === 'consult_p'
+                                ? <StateProject setShowSearch={setShowSearch} proList={proList} /> : operation === 'profil'
+                                    ? <Profil owner={props.owner} setOwner={props.setOwner} /> : null}
                     {showNotification &&
                         <div className='noti'>
                             <div className='notification'></div>
@@ -67,7 +67,7 @@ const Home = (props) =>
                     }
                     {showContact &&
                         <div className='contact'>
-                            
+
                         </div>
                     }
                 </section>
